b5f15fc3e2485dcc8a8cdbf2bb95aab39a194168
alexandre <alexandre.chevalier@laplateforme.io>
2025-04-03 04:22:32 +0200

    init sorting file with basic class structure

---
+++ sorting.py
@@ -0,0 +1,23 @@
+import random
+
+class Sorting():
+    def __init__(self,number):
+        self.number_in_table = number
+        self.table = self.table_creation()
+        
+
+    def table_creation(self):
+        self.table = [i for i in range(1 ,self.number_in_table)]
+        random.shuffle(self.table)
+        return self.table
+    
+    def insertion_sort(self):
+        print('insertion')
+    
+    def quicksort(self):
+        print('quick')
+
+
+    
+sort = Sorting(20)
+print(sort.table)
