53918f7795651bee48bff1ee4faeb1522f1cae56
alexandre <alexandre.chevalier@laplateforme.io>
2025-04-08 11:09:53 +0200

    every trik sort was implemented in the sorting class

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from sorting import Sorting
 
 
-sort = Sorting(10000000)
+sort = Sorting(100)
 sort.quicksort(sort.table_creation())
 sort.insertion_sort()
+sort.bubble_sort()
+sort.selection_sort()
+sort.peigne()
+sort.merge(sort.table_creation())
+sort.heap()
