f4d6bfbc9c165137f748bf0bd982123cdf9cf2fb
Kylliann <kylliann.larcher@laplateforme.io>
2025-04-08 10:10:25 +0200

    structure file

--- SRC/main.py
+++ SRC/main.py
@@ -1,11 +1,8 @@
-from SRC.display_tri import SortingVisualizer
+from display_tri import *
 from sorting import Sorting
 
 
 
 
-if __name__ == "__main__":
-    app = SortingVisualizer()
-    app.run()
-    sort = Sorting(100000000)
-    sort.quicksort(sort.table_creation())
+sort = Sorting(100000000)
+sort.quicksort(sort.table_creation())
